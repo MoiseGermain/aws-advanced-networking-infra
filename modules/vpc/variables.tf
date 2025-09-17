74d1683a2becfb27cde9393a7c815ee8972eb629
Moise Germain <60044518+MoiseGermain@users.noreply.github.com>
2025-09-17 14:34:14 -0400

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,1 +1,18 @@
+variable "hub_cidr" {
+  type        = string
+  default     = "10.0.0.0/16"
+  description = "CIDR block for the Hub VPC"
+}
+
+variable "spoke1_cidr" {
+  type        = string
+  default     = "10.1.0.0/16"
+  description = "CIDR block for Spoke 1 VPC"
+}
+
+variable "spoke2_cidr" {
+  type        = string
+  default     = "10.2.0.0/16"
+  description = "CIDR block for Spoke 2 VPC"
+}
 
