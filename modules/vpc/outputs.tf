12dcd1cedc86c3108f07401f23863ff4fb6c7e20
Moise Germain <60044518+MoiseGermain@users.noreply.github.com>
2025-09-17 14:33:50 -0400

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,1 +1,24 @@
+output "hub_vpc_id" {
+  value = aws_vpc.hub.id
+}
+
+output "spoke1_vpc_id" {
+  value = aws_vpc.spoke1.id
+}
+
+output "spoke2_vpc_id" {
+  value = aws_vpc.spoke2.id
+}
+
+output "hub_subnet_ids" {
+  value = [aws_subnet.hub_public.id]
+}
+
+output "spoke1_subnet_ids" {
+  value = [aws_subnet.spoke1_private.id]
+}
+
+output "spoke2_subnet_ids" {
+  value = [aws_subnet.spoke2_private.id]
+}
 
