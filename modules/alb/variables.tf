6c5770a6a66fc5398a5d71eeed3adf4fc43fc1c2
Moise Germain <60044518+MoiseGermain@users.noreply.github.com>
2025-09-17 14:37:24 -0400

    Update variables.tf

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,1 +1,15 @@
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID where the ALB will be deployed"
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  description = "List of public subnet IDs for ALB"
+}
+
+variable "certificate_arn" {
+  type        = string
+  description = "ACM certificate ARN for HTTPS listener"
+}
 
