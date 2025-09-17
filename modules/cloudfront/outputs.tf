70b1f29488aa821b109d617a8cf23d123999a36b
Moise Germain <60044518+MoiseGermain@users.noreply.github.com>
2025-09-17 14:38:59 -0400

    Update outputs.tf

--- modules/cloudfront/outputs.tf
+++ modules/cloudfront/outputs.tf
@@ -1,1 +1,8 @@
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.this.domain_name
+}
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.this.id
+}
 
