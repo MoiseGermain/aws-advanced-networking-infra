a73c2ea1edff960c678653620c397a698edb1c6e
Moise Germain <60044518+MoiseGermain@users.noreply.github.com>
2025-09-17 14:36:01 -0400

    Update variables.tf

--- modules/tgw/variables.tf
+++ modules/tgw/variables.tf
@@ -1,1 +1,30 @@
+variable "hub_vpc_id" {
+  type        = string
+  description = "Hub VPC ID"
+}
+
+variable "spoke1_vpc_id" {
+  type        = string
+  description = "Spoke 1 VPC ID"
+}
+
+variable "spoke2_vpc_id" {
+  type        = string
+  description = "Spoke 2 VPC ID"
+}
+
+variable "hub_subnet_ids" {
+  type        = list(string)
+  description = "Subnet IDs for Hub VPC"
+}
+
+variable "spoke1_subnet_ids" {
+  type        = list(string)
+  description = "Subnet IDs for Spoke 1 VPC"
+}
+
+variable "spoke2_subnet_ids" {
+  type        = list(string)
+  description = "Subnet IDs for Spoke 2 VPC"
+}
 
