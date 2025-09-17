960077056fbdcc3111882c606fb473ed57879967
Moise Germain <60044518+MoiseGermain@users.noreply.github.com>
2025-09-17 14:38:45 -0400

    Update variables.tf

--- modules/cloudfront/variables.tf
+++ modules/cloudfront/variables.tf
@@ -1,1 +1,16 @@
+variable "alb_dns_name" {
+  type        = string
+  description = "DNS name of the Application Load Balancer"
+}
+
+variable "certificate_arn" {
+  type        = string
+  description = "ACM certificate ARN for CloudFront distribution"
+}
+
+variable "waf_acl_arn" {
+  type        = string
+  description = "ARN of the WAF ACL to associate with CloudFront (optional)"
+  default     = ""
+}
 
