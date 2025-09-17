930011802f0495a86194b6adbee40be57d299fdb
Moise Germain <60044518+MoiseGermain@users.noreply.github.com>
2025-09-17 14:38:19 -0400

    Update main.tf

--- modules/cloudfront/main.tf
+++ modules/cloudfront/main.tf
@@ -1,1 +1,56 @@
+########################################
+# Origin Access Control (OAC) for ALB
+########################################
+resource "aws_cloudfront_origin_access_control" "this" {
+  name                              = "alb-oac"
+  description                       = "Access control for ALB origin"
+  origin_access_control_origin_type = "custom"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+########################################
+# CloudFront Distribution
+########################################
+resource "aws_cloudfront_distribution" "this" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origins {
+    domain_name = var.alb_dns_name
+    origin_id   = "alb-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+
+    origin_access_control_id = aws_cloudfront_origin_access_control.this.id
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "alb-origin"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      res
 
